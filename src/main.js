b9521e7bb55ac91056abc8e4ede61b833e964754
Vicente Vendramin <guesser.vicente@gmail.com>
2022-11-13 19:51:43 -0300

    adds sweetalert2 library and error handling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import './style.css';
+import Swal from 'sweetalert2';
 
 const coinInput = document.querySelector('#coin');
 const searchButton = document.querySelector('#search-coin');
 const main = document.querySelector('main');
 
 const BASE_URL = 'https://api.exchangerate.host/latest?base=';
+const SYMBOLS_URL = 'https://api.exchangerate.host/symbols';
+
+const fetchSymbols = await fetch(SYMBOLS_URL);
+const dataSymbols = await fetchSymbols.json();
+const symbols = Object.keys(dataSymbols.symbols);
 
 function renderCoins(data) {
   main.innerHTML = '';
@@ -22,15 +28,30 @@ function renderCoins(data) {
   });
 }
 
+const checkCoin = (coin) => symbols.some((symbol) => symbol === coin.toUpperCase());
+
 async function fetchAPI() {
   const coin = coinInput.value;
   coinInput.value = '';
 
-  const result = await fetch(`${BASE_URL}${coin}`);
-  const data = await result.json();
+  if (coin.length > 0) {
+    if (checkCoin(coin)) {
+      const result = await fetch(`${BASE_URL}${coin}`);
+      const data = await result.json();
 
-  console.log(data);
-  return renderCoins(data);
+      return renderCoins(data);
+    }
+    return Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'The currency does not exist',
+    });
+  }
+  return Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'You need to enter a currency',
+  });
 }
 
 searchButton.addEventListener('click', fetchAPI);
